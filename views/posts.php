594f06f822336d9331c1fe2924ade18ac3d1545d
ynswtf <senemyunus@gmail.com>
2018-09-07 22:48:43 +0300

    Post Style

--- views/posts.php
+++ views/posts.php
@@ -45,13 +45,13 @@
                                         </div>
                                         <h6 class="uk-margin-remove"><?= $post->user->username ?></h6>
                                         <p class="uk-text-small uk-margin-remove"><?= $post->user->email ?></p>
-                                        <div class="uk-text-small">
-                                            <?= __('%date%', ['%date%' => '<time datetime="'.$post->date->format(\DateTime::ATOM).'" v-cloak>{{ "'.$post->date->format(\DateTime::ATOM).'" | date "longDate" }}</time>' ]) ?>
-                                             - <a href=""><?= $post->category->title ?></a>
-                                        </div>
                                     </div>
                                 </div>
                             <?php endif; ?>
+                            <div class="uk-text-small">
+                                <?= __('%date%', ['%date%' => '<time datetime="'.$post->date->format(\DateTime::ATOM).'" v-cloak>{{ "'.$post->date->format(\DateTime::ATOM).'" | date "longDate" }}</time>' ]) ?>
+                                 - <a href=""><?= $post->category->title ?></a>
+                            </div>
                             <a href="<?= $view->url('@dpnblog/id', ['id' => $post->id]) ?>" class="dpnblog-readme"><?= __('Readme') ?></a>
                         </div>
                     </div>
@@ -68,4 +68,48 @@
             <hr />
         </div>
     <?php endforeach; ?>
+
+    <?php
+    $range     = 3;
+    $total     = intval($total);
+    $page      = intval($page);
+    $pageIndex = $page - 1;
+    ?>
+
+    <?php if ($total > 1) : ?>
+        <ul class="uk-pagination">
+
+
+            <?php for($i=1;$i<=$total;$i++): ?>
+                <?php if ($i <= ($pageIndex+$range) && $i >= ($pageIndex-$range)): ?>
+
+                    <?php if ($i == $page): ?>
+                    <li class="uk-active"><span><?=$i?></span></li>
+                    <?php else: ?>
+                    <li>
+                        <a href="<?= $view->url('@dpnblog/page', ['page' => $i]) ?>"><?=$i?></a>
+                    <li>
+                    <?php endif; ?>
+
+                <?php elseif($i==1): ?>
+
+                    <li>
+                        <a href="<?= $view->url('@dpnblog/page', ['page' => 1]) ?>">1</a>
+                    </li>
+                    <li><span>...</span></li>
+
+                <?php elseif($i==$total): ?>
+
+                    <li><span>...</span></li>
+                    <li>
+                        <a href="<?= $view->url('@dpnblog/page', ['page' => $total]) ?>"><?=$total?></a>
+                    </li>
+
+                <?php endif; ?>
+            <?php endfor; ?>
+
+
+        </ul>
+    <?php endif ?>
+
 </section>
